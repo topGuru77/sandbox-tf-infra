2bd41f701e668abc0e711c41a6fc4eb447bb7b60
A kwamena <kwamenadollar17@yahoo.com>
2025-04-02 14:29:26 -0400

    webhook testing in dev environment

--- main.tf
+++ main.tf
@@ -1,20 +1,29 @@
 #Creating web server for airbnb
 
-resource "aws_instance" "airbnb-web-server" {
+resource "aws_instance" "sandbox-web-server" {
   ami           =var.ami_id
   instance_type = var.instance_type
 
   tags = {
-    Name = "airbnb-web-server"
+    Name = "sandbox-web-server"
   }
 }
 
 #Creating prod server for airbnb
-resource "aws_instance" "airbnb-prod-server" {
+resource "aws_instance" "sandbox-prod-server" {
   ami           =var.ami_id
   instance_type = var.instance_type
 
   tags = {
-    Name = "airbnb-prod-server"
+    Name = "sandbox-prod-server"
+  }
+}
+
+resource "aws_instance" "sandbox-dev-server" {
+  ami           =var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "sandbox-dev-server"
   }
 }
