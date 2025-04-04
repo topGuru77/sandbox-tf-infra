2e681750b8e4db73046d69e44e97e5c370e749d9
A kwamena <kwamenadollar17@yahoo.com>
2025-04-03 21:40:50 -0400

    dynamodb table commented out

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
       key = "sandbox-infra/terraform.tfstate"
         region = "us-east-1"
         encrypt = true 
-        dynamodb_table = "terraform_aws-networking_state_lock"
+        # dynamodb_table = "terraform_aws-networking_state_lock"
   
   }
 }
