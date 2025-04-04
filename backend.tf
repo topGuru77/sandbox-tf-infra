67fd54c6b9c721c922adf68685bf3a7261e59436
A kwamena <kwamenadollar17@yahoo.com>
2025-04-03 20:56:31 -0400

    backend file added

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
-      bucket = "sandbox-devops-infra "
-      key = "sanddbox-infra/terraform.tfstate"
+      bucket = "sandbox-devops-infra"
+      key = "sandbox-infra/terraform.tfstate"
         region = "us-east-1"
         encrypt = true 
         dynamodb_table = "terraform_aws-networking_state_lock"
